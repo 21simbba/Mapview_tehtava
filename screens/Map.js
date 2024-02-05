299b2a892976f8868913aaf3dba9e683d6be455b
simo vattulainen <simo.vattulainen@gmail.com>
2024-02-05 14:03:09 +0200

    appbar lisätty

--- screens/Map.js
+++ screens/Map.js
@@ -1,43 +1,16 @@
 import MapView from 'react-native-maps'
 import { StyleSheet, Text, View } from 'react-native';
 import { useEffect, useState } from 'react';
-import * as Location from 'expo-location'
 
-export default function Map() {
 
-    const [location, setLocation] = useState({
-        latitude: 69.0800,
-        longitude: 13.4800,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-    })
+export default function Map(props) {
 
-    const getUserPosition = async () => {
-        let {status} = await Location.requestForegroundPermissionsAsync()
-        try{
-            if (status !== 'granted'){
-                console.log('Geolocation Failed')
-                return
-            }
-            const position = await Location.getCurrentPositionAsync({accuracy: Location.Accuracy.High})
-            setLocation({...location,"latitude":position.coords.latitude,"longitude":position.coords.longitude})
-        } catch (error) {
-            console.log(error)
-        }
-
-    }
-    
-    useEffect(() => {
-        (async() => {
-            getUserPosition()
-        }) ()
-    }, [])
-    
   return (
    <>
     <MapView
     style={styles.map}
-    region={location}
+    region={props.location}
+    mapType='satellite'
     />
    </>
   );
